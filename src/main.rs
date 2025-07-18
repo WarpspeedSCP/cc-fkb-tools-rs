e8fc3f1c73bb37611de6a783fee8655ac42aa76a
Raghav Shankar <warpspeedscp@gmail.com>
2025-07-18 12:21:59 +0530

    initial changes

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,88 @@
 mod util;
 
-use core::ptr::addr_of;
-use std::{collections::HashMap, io::Read, path::PathBuf};
+use std::{collections::HashMap, env::current_dir, fs::create_dir_all, path::{Path, PathBuf}, process::exit};
+use serde_derive::{Deserialize, Serialize};
+use util::{get_sjis_bytes, get_sjis_bytes_of_length, transmute_to_u32};
+use walkdir::WalkDir;
 
-use util::{get_sjis_bytes, get_sjis_bytes_of_length, transmute_to_u16, transmute_to_u32};
+use crate::util::encode_sjis;
+
+fn get_final_file_list<T>(input_files: Vec<T>, recurse_dirs: bool, action: &str) -> Vec<PathBuf>
+where
+  T: AsRef<Path> + Sync + Send,
+{
+  let filter_func = |it: &PathBuf| {
+    let value = it.extension().unwrap_or_default();
+    if it.file_stem().unwrap_or_default() == "directory" {
+      return false;
+    }
+    if action == "decode" {
+      value == "opcodescript" || value == "yaml"
+    } else if action == "transform" {
+      value == "yaml"
+    } else {
+      true
+    }
+  };
+
+  use rayon::prelude::*;
+  let output = if !recurse_dirs {
+    input_files
+      .par_iter()
+      .map(|it| it.as_ref().to_path_buf())
+      .filter(filter_func)
+      .collect()
+  } else {
+    input_files
+      .par_iter()
+      .flat_map(|file| {
+        WalkDir::new(file.as_ref())
+          .contents_first(true)
+          .into_iter()
+          .filter_map(Result::ok)
+          .filter(|it| it.path().is_file())
+          .map(|it| it.into_path())
+          .filter(filter_func)
+          .collect::<Vec<_>>()
+      })
+      .collect()
+  };
+
+  output
+}
 
 fn main() {
   env_logger::builder().format_timestamp(None).init();
-  std::fs::create_dir_all("unpacked_arcs").unwrap();
+  let args = std::env::args().collect::<Vec<_>>();
+
+  if args.contains(&"extract_all".to_string()) {
+    extract_all_arcs();
+  } else if args.contains(&"extract".to_string()) {
+    let arc_name = &args[2];
+  
+    if args[3] != "--output" && args[3] != "-o" {
+      log::error!("specify output argument as -o/--output, followed by the output file.");
+      exit(1);
+    }
+  
+    let out_dir = &args[4];
+
+    let out_dir_path = current_dir()
+        .unwrap()
+        .join(out_dir)
+        .canonicalize()
+        .unwrap();
+
+    create_dir_all(&out_dir_path).unwrap();
+
+    
+    
+  } 
+
+}
+
+fn extract_all_arcs() {
+    std::fs::create_dir_all("unpacked_arcs").unwrap();
   let files = walkdir::WalkDir::new("/home/wscp/cc-fkb-tools/cross channel fkb/").into_iter().filter_map(|it| it.ok()).collect::<Vec<_>>();
   for i in files.iter() {
     let dirent = i;
@@ -19,11 +94,8 @@ fn main() {
     
     let path = PathBuf::from("unpacked_arcs").join(dirent.file_name());
     std::fs::create_dir_all(&path).unwrap();
-    let out = read_arc(&mut file[..], path);
-
+    read_arc(&mut file[..], path, false);
   }
-
-
 }
 
 #[repr(C, packed)]
@@ -104,6 +176,7 @@ impl WIPFENTRY {
   }
 }
 
+#[derive(Serialize, Deserialize)]
 struct ExtensionDescriptor {
   name: String,
   number: u32,
@@ -120,6 +193,7 @@ impl ExtensionDescriptor {
   }
 }
 
+#[derive(Serialize, Deserialize)]
 struct FileDescriptor {
   name: String,
   size: usize,
@@ -136,7 +210,50 @@ impl FileDescriptor {
   }
 }
 
-fn read_arc(input: &mut [u8], out_folder: PathBuf) -> () {
+fn write_arc(input_folder: &Path) -> Vec<u8> {
+  let mut output = vec![];
+  
+  let extensions = 
+  std::fs::read_to_string(input_folder.join("extensions.json"))
+    .iter().flat_map(|str| serde_json::from_str::<Vec<ExtensionDescriptor>>(&str))
+    .next().unwrap_or_default();
+  let files = std::fs::read_to_string(input_folder.join("files.json"))
+    .iter().flat_map(|str| serde_json::from_str::<Vec<(String, FileDescriptor)>>(&str))
+    .next().unwrap_or_default();
+  
+  for descriptor in extensions {
+    output.extend(encode_sjis(&descriptor.name));
+    output.push(0);
+    output.extend(&descriptor.number.to_le_bytes());
+    output.extend(&descriptor.offset.to_le_bytes());
+  }
+
+  let mut things_to_append = vec![];
+  let mut curr_offset = output.len();
+
+  for (filename, descriptor) in files {
+    output.extend(encode_sjis(&descriptor.name));
+    output.push(0);
+    let curr_path = input_folder.join(&filename);
+    let mut contents = std::fs::read(&curr_path).unwrap();
+    
+    if filename.ends_with("WSC") {
+      contents.iter_mut().for_each(|chr| *chr = chr.rotate_left(2));
+    }
+
+    output.extend(&(curr_offset as u32).to_le_bytes());
+    output.extend(&(contents.len() as u32).to_le_bytes());
+    curr_offset += contents.len();
+    
+    things_to_append.push(contents);
+  }
+
+  things_to_append.iter().for_each(|it| output.extend(it));
+
+  output
+}
+
+fn read_arc(input: &mut [u8], out_folder: PathBuf, extract_wipf: bool, ) -> () {
   let n_ext_descriptors = transmute_to_u32(0, input);
 
   let mut ext_descriptors = vec![];
@@ -159,14 +276,17 @@ fn read_arc(input: &mut [u8], out_folder: PathBuf) -> () {
     });
   }
 
+  let ext_descriptors_out_file = out_folder.join("extensions.json");
+  std::fs::write(&ext_descriptors_out_file, serde_json::to_string_pretty(&ext_descriptors).unwrap()).unwrap();
 
   log::info!("There are {} files to process.", ext_descriptors.iter().map(|it| it.number).sum::<u32>());
 
-  let mut files = HashMap::new();
+  let mut files = vec![];
 
   for descriptor in ext_descriptors.iter() {
     let start_addr = descriptor.offset as usize;
-    let mut descriptor_ptr = start_addr;
+    let mut descriptor_ptr = start_addr;  let ext_descriptors_out_file = out_folder.join("extensions.json");
+  std::fs::write(&ext_descriptors_out_file, serde_json::to_string_pretty(&ext_descriptors).unwrap()).unwrap();
     for _ in 0..descriptor.number {
       let (name, unicode) = get_sjis_bytes_of_length(descriptor_ptr, 13, input);
       descriptor_ptr += name.len() - 1;
@@ -175,12 +295,15 @@ fn read_arc(input: &mut [u8], out_folder: PathBuf) -> () {
       let offset = transmute_to_u32(descriptor_ptr, input);
       descriptor_ptr += 4;
       log::info!("File {unicode}.{} of size 0x{size:08X} starts at 0x{offset:08X}", descriptor.name.as_str());
-      files.insert(
+      files.push((
         format!("{}.{}", unicode, descriptor.name),
         FileDescriptor::new(unicode, size, offset)
-      );
+      ));
     }
   }
+
+  let file_descriptors_out_file = out_folder.join("files.json");
+  std::fs::write(&file_descriptors_out_file, serde_json::to_string_pretty(&files).unwrap()).unwrap();
 
   for (filename, desc) in files {
     log::info!("Processing {filename}.");
@@ -191,7 +314,7 @@ fn read_arc(input: &mut [u8], out_folder: PathBuf) -> () {
 
     if filename.ends_with("WSC") {
       content.iter_mut().for_each(|chr| *chr = chr.rotate_right(2));
-    } else if &content[..4] == "WIPF".as_bytes() {
+    } else if &content[..4] == "WIPF".as_bytes() && extract_wipf {
       let header = WIPFHeader::from_ref(content);
       let entries = WIPFENTRY::from_ref_as_slice(
         &content[size_of_val(header)..],
@@ -200,7 +323,6 @@ fn read_arc(input: &mut [u8], out_folder: PathBuf) -> () {
 
       log::warn!("WIPF file {filename} has {} entries with depth {}.", entries.len(), u32::from(header.depth));
       
-      /*
       let data = &content[size_of_val(header) + size_of_val(entries)..];
       let mut data_ptr = 0usize;
       for (entry_no, entry) in entries.iter().enumerate() {
@@ -216,43 +338,12 @@ fn read_arc(input: &mut [u8], out_folder: PathBuf) -> () {
         let out_stride = (entry.width * out_depth + 3) & !3u32;
         let out_len = (entry.height * out_stride) as usize;
 
-        let out_buf = lz77_decompress(&data[data_ptr..(data_ptr + entry.length as usize)], out_len);
+        let out_buf = unwipf(&data[data_ptr..(data_ptr + entry.length as usize)], out_len);
 
         data_ptr += entry.length as usize;
 
         
         let out_file = output_file_path.join(&format!("{filename}_{entry_no:03}+{}x{}y.bmp", u32::from(entry.x_offset), u32::from(entry.y_offset)));
-
-        /*
-        					if( wipfhdr -> depth == 24 ) { // 24位图
-						unsigned char *temp_buff = new unsigned char[ out_len ];
-						
-						unsigned long color_stride = wipfentries[ k ].width;
-							// RGB色间距 = 宽
-						unsigned long color_len    = wipfentries[ k ].height * color_stride;
-							// 高 x 宽 （因为是RGB三色的，所以一行图是一色）
-
-						for( unsigned long y = 0; y < wipfentries[ k ].height; y ++ ) {
-							// 行间循环
-							struct RGB { unsigned char r, g, b; };
-							RGB *rgb_line = (RGB *)( temp_buff + y * out_stride );
-								// temp_buff[ y * out_stride ] 定义的是指针
-							unsigned char *r_line = out_buff + y * color_stride;
-							unsigned char *g_line = r_line + color_len;
-							unsigned char *b_line = g_line + color_len;
-
-							for( unsigned long x = 0; x < wipfentries[ k ].width; x ++ ) {
-								// 列循环（行内循环）rgb_line[ ]是一行内的内容
-								rgb_line[ x ].r = r_line[ x ];
-								rgb_line[ x ].g = g_line[ x ];
-								rgb_line[ x ].b = b_line[ x ];
-							}
-						}
-
-						delete [ ] out_buff;
-						out_buff = temp_buff;
-					}
-         */
 
         let mut out_buf = if header.depth == 24 {
           let mut new_out = vec![0u8; out_buf.len()];
@@ -289,15 +380,6 @@ fn read_arc(input: &mut [u8], out_folder: PathBuf) -> () {
           out_buf
         };
 
-        /*
-        	for( unsigned int rr = 0; rr < wipfentries[ k ].height / 2; rr ++ ) {
-						for( unsigned int ee = 0; ee < wipfentries[ k ].width * out_depth; ee ++ ) {
-							swap( out_buff[ ( wipfentries[ k ].height - rr - 1 ) * wipfentries[ k ].width * out_depth + ee ],
-								  out_buff[ rr * wipfentries[ k ].width * out_depth + ee ] );
-						}
-					}
-         */
-
         for i in 0..(entry.height / 2) as usize {
           for j in 0..(entry.width * out_depth) as usize {
             let a = out_buf[(entry.height as usize - i - 1) * entry.width as usize * out_depth as usize + j];
@@ -330,14 +412,19 @@ fn read_arc(input: &mut [u8], out_folder: PathBuf) -> () {
             offset: bmp_offset,
         };
 
-        let mut bmp_dib_header: BMP_Dib_V3_Header = unsafe { std::mem::transmute([0u8; size_of::<BMP_Dib_V3_Header>()]) };
-
-        bmp_dib_header.header_sz = 0x28;
-        bmp_dib_header.width = entry.width;
-        bmp_dib_header.height = entry.height;
-        bmp_dib_header.depth = header.depth;
-        bmp_dib_header.nplanes = 1;
-        bmp_dib_header.bmp_bytesz = imgdata_size as u32;
+        let bmp_dib_header = BMP_Dib_V3_Header {
+          header_sz: 0x28,
+          width: entry.width,
+          height: entry.height,
+          nplanes: 1,
+          bmp_bytesz: imgdata_size as u32,
+          depth: header.depth,
+          compress_type: 0,
+          hres: 0,
+          vres: 0,
+          ncolors: 0,
+          nimpcolors: 0,
+        };
 
         std::fs::create_dir_all(&output_file_path).unwrap();
         let hdr_bytes = to_bytes(&bmp_header);
@@ -354,7 +441,6 @@ fn read_arc(input: &mut [u8], out_folder: PathBuf) -> () {
           ).unwrap();
       }
       continue;
-      */
     }
 
     std::fs::write(output_file_path, content).unwrap();
@@ -368,6 +454,108 @@ pub fn to_bytes<T: Sized> (value: &T) -> &[u8] {
   }
 }
 
+// unsigned long CrossChannelCrack::unwipf( unsigned char* buff,      // 输入文件正文的array
+// 										 unsigned long  len,       // 输入文件长度
+// 										 unsigned char* out_buff,  // 输出文件正文的array
+// 										 unsigned long  out_len )  // 输出文件长度
+// {
+pub fn unwipf(input: &[u8], out_len: usize) -> Vec<u8> {
+  let mut buff = 0usize;
+  
+  // 	unsigned long  ring_len   = 4096;                        // 寻址范围就是0-4095
+  let ring_len = 4096;
+  // 	unsigned char* ring       = new unsigned char[ring_len]; // 剪贴板
+  let mut ring = [0u8; 4096];
+  // //	unsigned long ring_index  = 0xFEE;                       // 剪贴板的索引
+  // 	unsigned long  ring_index = 1;                           // 剪贴板的索引
+  let mut ring_index = 0xFEEusize;
+  // 	unsigned char* end        = buff + len;                  // 指向输入array的末尾
+  let end = input.len();
+  
+  let mut out = vec![0u8; out_len];
+  let mut out_buff = 0usize;
+  // 	unsigned char* out_end    = out_buff + out_len;          // 指向输出array的末尾
+  let out_end = out.len();
+  
+
+  // 	memset(ring, 0, ring_len); // 剪贴板 置零
+
+  // 	while (buff < end && out_buff < out_end) { // 判断条件：不超过array的末尾
+  while buff < end && out_buff < out_end {
+    // 		unsigned char flags = *buff++; //<1> 取当前字符存到flags里面
+    let mut flags = input[buff];
+    buff += 1;
+    // 		// 这个循环是判断8位的char型的每一位是否为1
+    // 		for (int i = 0; i < 8 && buff < end && out_buff < out_end; i++) { // 判断条件：char型长度8循环&&不超过array的末尾
+    for i in 0..8 {
+      if buff >= end || out_buff >= out_end {
+        break
+      }
+      // 			if (flags & 0x01) { // 判断：最低位是否为1
+      if flags & 1 == 1 {
+        // 			/*
+        // 			*out_buff = ring[ring_index % ring_len] = *buff++; // 从ring[ 1 ]开始赋值到[ 0 ]，以4096循环
+        out[out_buff] = ring[ring_index % 4096];
+        // 			ring_index++;
+        ring_index += 1;
+        // 			*out_buff++;
+        out_buff += 1;
+      // 			*/
+        // 		    *out_buff++ = ring[ring_index++ % ring_len] = *buff++; //<2>
+        // 			} else {
+      } else {
+        // 				if (end - buff < 2) break;
+        if (end - buff) < 2 {
+          break;
+        }
+        // 				unsigned long p = *buff++; //<3>
+        let p = input[buff] as u64;
+        buff += 1;
+        // 				unsigned long n = *buff++; //<4>
+        let n = input[buff] as u64;
+        buff += 1;
+        // 				//p = p | ( ( n >> 4 ) << 8 );
+        // 				p = (p << 4) | (n >> 4);   // ( RHL p, 4 ) OR ( RHR n, 4 ) 即 p的八位 与 n的前四位 构成
+        let mut p1 = (p << 4) | (n >> 4);
+        // 				n = (n & 0x0F) + 2;        // AND 00001111b; ADD 2 这里用到的是n的后四位
+        let n1 = (n & 0x0F) + 2;
+        // 				for (unsigned long j = 0; j < n && out_buff < out_end; j++) {
+        for _ in 0..n1 {
+          if out_buff >= out_end {
+            break;
+          }
+        // 				//for (unsigned long j = 0; j <= n && out_buff < out_end; j++) {
+        // 					/*
+        // 					*out_buff = ring[ring_index % ring_len] = ring[p % ring_len];
+          ring[ring_index % 4096] = ring[(p1 as usize) % 4096];
+          out[out_buff] = ring[ring_index % 4096];
+          // 					p++;
+          p1 += 1;
+        // 					ring_index++;
+          ring_index += 1;
+        // 					*out_buff++;
+          out_buff += 1;
+        // 					*/
+        // 					*out_buff++ = ring[ring_index++ % ring_len] = ring[p++ % ring_len];
+        // 				}
+        }
+        // 			}
+      }
+      // 			flags >>= 1; // 右移1位，进入下一轮判断
+      flags >>= 1;
+      // 		}
+    }
+    // 	}
+  }
+
+  out
+  // 	delete [] ring;
+
+  // 	return out_len - (out_end - out_buff); // 忽略掉的字符数量
+  // }
+}
+
+// This algorithm is just ever so slightly mismatched with the one which actually applies to the mask pngs.
 pub(crate) fn lz77_decompress(input: &[u8], size: usize) -> Vec<u8> {
   let mut output = vec![0; size as usize];
 
